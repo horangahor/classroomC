2c4cc0e2343793b77f08879cfb23b683f56006e0
horangahor <hkl8025@naver.com>
2025-07-30 13:08:14 +0900

    파일 형식 체크 수정

--- political_summary_system/node/router/adminRouter.js
+++ political_summary_system/node/router/adminRouter.js
@@ -26,11 +26,19 @@ const upload = multer({
 
 router.post('/upload',upload.single('file'), async(req, res)=>{
     
-    console.log(req.filename);
+    const fileType = req.file.originalname.slice(-3,);
+    console.log(fileType);
+
+    if(fileType != "csv"){
+        res.status(415).json({message : "파일 형식 맞지 않음"});
+    }
+    
     const filePath = path.join(__dirname, "..", "dataset", req.file.filename);
+    console.log(filePath);
+
     try{
         const csvData = await csvReader.readFile(filePath);
-        console.log("get : ", csvData);
+        // console.log("get : ", csvData);
 
 
         const insertionPromises = csvData.map(data => insertMember(data.name, data.age, data.position, data.politics));
@@ -39,17 +47,18 @@ router.post('/upload',upload.single('file'), async(req, res)=>{
         // for(i=0;i<csvData.length;i++){
         //     await insertMember(csvData[i].name,csvData[i].age,csvData[i].position,csvData[i].politics );
         // }
+        res.status(200).json({ message: "파일 업로드 및 처리 성공", status: "ok" });
     }
     catch(err){
         console.error(err);
+        res.status(500).json({message: "알 수 없는 오류"});
     }
     finally{
         fs.unlink(filePath, (err) => {
             if(err){
-                error(err);
+                console.error(err);
             }
         });
-        res.status(200).json({ message: "파일 업로드 및 처리 성공", status: "ok" });
     }
 })
 
