81ee46202ad4f8fe63814bc1045dadc955a9cba3
mmooeom <koouo3387@gmail.com>
2025-07-31 09:54:52 +0900

    회원가입, 로그인 기능 추가

--- political_summary_system/node/database/db.js
+++ political_summary_system/node/database/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
     host : '192.168.111.216',
-    user : 'hanmadi',
+    user : 'hanmadi', 
     password : 'hanmadi1234',
     port : 3306,
     database : 'politician'
