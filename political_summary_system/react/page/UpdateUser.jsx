20a1c81a1a277debccd237feaa8ca634852bed6f
mmooeom <koouo3387@gmail.com>
2025-08-04 17:33:22 +0900

    문제가 생겼어요.

--- political_summary_system/react/page/UpdateUser.jsx
+++ political_summary_system/react/page/UpdateUser.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import '../style/UpdateUser.css'
-import axios from 'axios'
+
 
 const UpdateUser = () => {
     const navigate = useNavigate()
