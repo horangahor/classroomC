881cc0da836d00daec4d06795e593fa10c7b7a9b
Yuni00ni <moonrit001@gmail.com>
2025-08-05 16:12:20 +0900

    페이지 디자인 다량 수정

--- political_summary_system/react/page/Login.jsx
+++ political_summary_system/react/page/Login.jsx
@@ -28,18 +28,19 @@ const Login = () => {
     }
 
     axios
-            .post('http://localhost:8000/login', {
-                id : formData.email,
-                pw : formData.password,
-            })
-            .then ((res) =>{
-              console.log("이건은 response",res);
-                alert(`${res.data.nick} 로그인 성공!`)
-                nav('/');
-            })
-            .catch((err)=>{
-                console.error(err);
-            })
+      .post('http://localhost:8000/login', {
+        id: formData.email,
+        pw: formData.password,
+      })
+      .then((res) => {
+        // console.log("이건은 response", res);
+        alert(`${res.data.nick} 로그인 성공!`)
+        nav('/');
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('로그인 실패');
+      })
   }
 
   return (
